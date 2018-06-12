2c0ca5ced576975121b85b5d3e4519de041f2d80
amishak <andriy.mishak@gmail.com>
2018-06-12 18:42:48 +0200

    toString and equals

--- homework/src/main/java/Client.java
+++ homework/src/main/java/Client.java
@@ -39,24 +39,29 @@ public class Client {
         return result;
     }
 
-    @Override
-    public String toString() {
-        return id + ": " + name + " " + surname;
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Client{" +
+                "isRegular=" + isRegular +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", id=" + id +
+                '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null) {
-            return false;
-        }
-        if (this == o) {
-            return true;
-        }
-        if (o instanceof Client) {
-            Client toCompare = (Client)o;
-            return (this.email.equals(toCompare.email)
-                    && this.name.equals(toCompare.name));
-        }
-        return false;
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Client client = (Client) object;
+        return email.equals(client.email) &&
+                phone.equals(client.phone) &&
+                isRegular == client.isRegular &&
+                age == client.age &&
+                name.equals(client.name) &&
+                surname.equals(client.surname);
     }
 }
