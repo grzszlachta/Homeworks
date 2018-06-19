bf59d0692f06ff7c0d2fcec2f3d5b5a5785fe40b
amishak <genious@bigmir.net>
2018-06-19 10:36:56 +0200

    Homework 9.2

--- homework/src/main/java/model/Employee.java
+++ homework/src/main/java/model/Employee.java
@@ -8,7 +8,20 @@ public class Employee extends Person {
         super(age, _email, _name, _surname, _phone);
     }
 
+    @Override
     public int getDiscount() {
+        System.out.println("Discount of employee is 25");
         return 25;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Employee{" +
+                "age=" + age +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
     }
 }
--- homework/src/test/java/service/ClientServiceTest.java
+++ homework/src/test/java/service/ClientServiceTest.java
@@ -14,7 +14,7 @@ public class ClientServiceTest {
     @Test
     public void testAddClient() {
         ClientService service = new ClientService();
-        service.addClient(29, "a@email.com", "Ivan", "Ivanov", "+4812345678");
+        service.addClient(29,  "a@email.com", "Ivan", "Ivanov", "+4812345678");
         Assert.assertEquals(10, service.getStorageSize());
         Assert.assertEquals(1, countPersons(service.getClients()));
     }
@@ -33,9 +33,27 @@ public class ClientServiceTest {
         Assert.assertEquals(40, service.getStorageSize());
     }
 
+    @Test
+    public void testAverageDiscount() {
+        ClientService service = new ClientService();
+        service.addClient(29, false,"a@email.com", "Ivan", "Ivanov", "+4812345678");
+        service.addClient(35, true, "b@outlook.com", "Jan", "Kowalsky", "+4898765432");
+        service.addClient(17, true, "c@gmail.com", "John", "Smith", "+482345678");
+        service.addClient(17, true, "c@gmail.com", "John", "Smith", "+482345678");
+        service.addEmployee(29, "a@email.com", "Ivan", "Ivanov", "+4812345678");
+        int sum = 0;
+        for (Person person : service.getClients()) {
+            if (person != null) {
+                System.out.println("person = " + person);
+                sum += person.getDiscount();
+            }
+        }
+        System.out.println("Average discount = " + (float)sum / 5);
+    }
+
     private void generateRandomClient(ClientService service) {
-        int maybePhomeNumber = random.nextInt();
-        int phoneNumber = maybePhomeNumber > 0 ?  maybePhomeNumber : (maybePhomeNumber * -1) ;
+        int maybePhoneNumber = random.nextInt();
+        int phoneNumber = maybePhoneNumber > 0 ?  maybePhoneNumber : (maybePhoneNumber * -1) ;
         service.addClient(random.nextInt(83) + 1,
                 UUID.randomUUID().toString().replaceAll("-", "") + "@" +
                         UUID.randomUUID().toString().replaceAll("-", "") + ".com",
