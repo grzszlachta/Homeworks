5c16c0cbaff62fa17809c7cc356d7aa3415b383d
amishak <genious@bigmir.net>
2018-07-27 14:38:54 +0200

    Homework 20

---
+++ homework/src/main/java/docs/HomeWork20.md
@@ -0,0 +1,7 @@
+## HomeWork 20
+
+1. Fix stream in **StreamsTest#testStreamGenerateLimit** test to have limited ammount of output - first 10 integers  
+2. Fix stream in **StreamsTest#testStreamSort** test to have sorted output  
+3. Fix stream in **StreamsTest#testFilterToCapitalize** test to have only capitalized elements on exit  
+4. Fix stream in **StreamsTest#testMapToCapitalize** test to transform all elements to capitalized on exit  
+
---
+++ homework/src/test/java/tools/StreamsTest.java
@@ -0,0 +1,77 @@
+package tools;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamsTest {
+
+    @Test(timeout = 1000)
+    public void testStreamGenerateLimit() {
+        Supplier<Integer> integerSupplier = () -> new Random().nextInt(100);
+        Stream
+                .generate(integerSupplier)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void testStreamSort() {
+        List<String> listOfRandomStrings = Stream
+                .generate(() -> generateRandomString(new Random(), 9))
+                .peek(System.out::println)
+                .limit(25)
+                .collect(Collectors.toList());
+        List<String> toSort = new ArrayList<>(listOfRandomStrings);
+        Collections.sort(toSort, Collections.reverseOrder());
+        System.out.println("listOfRandomStrings = " + listOfRandomStrings);
+        System.out.println("toSort = " + toSort);
+        Assert.assertArrayEquals(listOfRandomStrings.toArray(), toSort.toArray());
+    }
+
+    @Test
+    public void testFilterToCapitalize() {
+        List<String> listOfRandomStrings = Stream
+                .generate(() -> generateRandomString(new Random(), 9))
+                .peek(System.out::println)
+                .limit(25)
+                .collect(Collectors.toList());
+        listOfRandomStrings.forEach(
+                o -> Assert.assertTrue(
+                        o + " is not capitalized",
+                        o.substring(0,1)
+                                .equals(o.substring(0,1).toUpperCase()))
+        );
+    }
+
+    @Test
+    public void testMapToCapitalize() {
+        int capacity = 12;
+        List<String> listOfRandomStrings = Stream
+                .generate(() -> generateRandomString(new Random(), 9))
+                .peek(System.out::println)
+                .limit(capacity)
+                .collect(Collectors.toList());
+        listOfRandomStrings.forEach(
+                o -> Assert.assertEquals(o + " is not capitalized",
+                        o.substring(0, 1), o.substring(0, 1).toUpperCase())
+        );
+        Assert.assertEquals(listOfRandomStrings.size(), capacity);
+    }
+
+    private static String generateRandomString(Random random, int length){
+        return random.ints(48,122)
+                .filter(i-> (i<57 || i>65) && (i <90 || i>97))
+                .mapToObj(i -> (char) i)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+
+}
