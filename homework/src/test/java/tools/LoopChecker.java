cb23bfc9cba644377d178906264329282a6d75f9
amishak <genious@bigmir.net>
2018-08-07 16:57:00 +0200

    Solution 22

---
+++ homework/src/test/java/tools/LoopChecker.java
@@ -0,0 +1,25 @@
+package tools;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class LoopChecker {
+
+    public static boolean hasLoopHashes(LinkedList<Object> input) {
+        Iterator<Object> iterator = input.iterator();
+        Map<Object, Integer> keyStorage = new HashMap<>();
+        while (iterator.hasNext()) {
+            Object cursor = iterator.next();
+            Integer hash = cursor.hashCode();
+            if (keyStorage.values().contains(hash)) {
+                return true;
+            } else {
+                keyStorage.put(cursor, hash);
+            }
+        }
+        return false;
+    }
+
+}
